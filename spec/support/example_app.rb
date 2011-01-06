1b3020fe70de28660af5c4c454bcfba2bc42ab84
Myron Marston <myron.marston@gmail.com>
2011-01-06 10:45:23 -0800

    Allow errors to be propagated.

--- spec/support/example_app.rb
+++ spec/support/example_app.rb
@@ -3,6 +3,8 @@ require 'erb'
 
 class ExampleApp < Sinatra::Base
   set :views, File.dirname(__FILE__) + '/views'
+  set :raise_errors, true
+  set :show_exceptions, false
 
   helpers MozNav::RenderHelpers
 
