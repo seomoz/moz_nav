0722640cafcc6e897089d092526ab493f784aeb8
Myron Marston <myron.marston@gmail.com>
2011-01-06 15:26:28 -0800

    Raise errors when a mustache variable/method isn't found.

--- lib/moz_nav/mustache_base.rb
+++ lib/moz_nav/mustache_base.rb
@@ -5,6 +5,12 @@ module MozNav
     self.view_path      = MozNav::Root + 'moz_nav/views'
     self.template_path  = MozNav::Root + 'moz_nav/templates'
     self.view_namespace = MozNav
+
+    def self.inherited(subclass)
+      # for some reason, this setting isn't properly inherited, so we need to
+      # set it directly on the subclass
+      subclass.raise_on_context_miss = true
+    end
   end
 end
 
