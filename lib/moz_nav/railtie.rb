2e5481133e3d468bf9cb1e6ab492fefe070c188b
Myron Marston <myron.marston@gmail.com>
2011-01-19 13:24:36 -0800

    Use Rails.public_path.

--- lib/moz_nav/railtie.rb
+++ lib/moz_nav/railtie.rb
@@ -4,7 +4,7 @@ require 'moz_nav'
 module MozNav
   class Railtie < Rails::Railtie
     initializer 'moz_nav.symlink_assets' do
-      MozNav.ensure_assets_symlinked_from(Rails.root + '/public')
+      MozNav.ensure_assets_symlinked_from(Rails.public_path)
     end
   end
 end
