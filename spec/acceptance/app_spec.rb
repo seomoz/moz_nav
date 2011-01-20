ad42c4f7cad09308b1edcb2488770ddfa0941f59
Myron Marston <myron.marston@gmail.com>
2011-01-20 08:58:56 -0800

    Updated shared example group to test the expected campaign interface.

--- spec/acceptance/app_spec.rb
+++ spec/acceptance/app_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 
 describe ExampleApp do
-  it_behaves_like 'the MozNav rendering context' do
+  it_behaves_like 'the MozNav rendering context', :campaign => true do
     let(:render_context) do
       described_class.new.tap do |c|
-        c.instance_eval { @current_user = User.new(17, :pro) }
+        c.instance_eval do
+          @current_user = User.new(17, :pro)
+          @current_campaign = Campaign.all.first
+        end
       end
     end
   end
