2eabe1185da797fa39cb1a4e546fd6fb07c1b010
Jeff Pollard <jeff.pollard@gmail.com>
2011-01-20 14:59:53 -0800

    Compass config file.

---
+++ config.rb
@@ -0,0 +1,9 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "lib/moz_nav/assets/stylesheets"
+sass_dir = "lib/moz_nav/assets/sass"
+images_dir = "lib/moz_nav/assets/images"
+javascripts_dir = "lib/moz_nav/assets/javascripts"
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
