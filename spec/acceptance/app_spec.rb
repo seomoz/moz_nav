5964381c947d2fbca578660bb3587439b76066e0
Myron Marston <myron.marston@gmail.com>
2011-01-20 15:59:20 -0800

    Revert "Updated shared example group to test the expected campaign interface."
    This reverts commit ad42c4f7cad09308b1edcb2488770ddfa0941f59.
    
    We actually don't need any particular current_campaign interface after
    all.

--- spec/acceptance/app_spec.rb
+++ spec/acceptance/app_spec.rb
@@ -1,13 +1,10 @@
 require 'spec_helper'
 
 describe ExampleApp do
-  it_behaves_like 'the MozNav rendering context', :campaign => true do
+  it_behaves_like 'the MozNav rendering context' do
     let(:render_context) do
       described_class.new.tap do |c|
-        c.instance_eval do
-          @current_user = User.new(17, :pro)
-          @current_campaign = Campaign.all.first
-        end
+        c.instance_eval { @current_user = User.new(17, :pro) }
       end
     end
   end
