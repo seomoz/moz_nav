c7ba00f12167b11de3c60333ebe47ebaae48c326
Jeff Pollard <jeff.pollard@gmail.com>
2011-01-20 15:03:59 -0800

    Move config for CSS for compass to the development CSS folder

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "lib/moz_nav/assets/stylesheets"
+css_dir = "lib/moz_nav/assets/stylesheets/development"
 sass_dir = "lib/moz_nav/assets/sass"
 images_dir = "lib/moz_nav/assets/images"
 javascripts_dir = "lib/moz_nav/assets/javascripts"
