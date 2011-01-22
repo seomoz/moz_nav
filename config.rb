65bec166345cbf7b2014c8430ba8a6ef5580d6b6
Jeff Pollard <jeff.pollard@gmail.com>
2011-01-21 19:06:15 -0800

    Good stopping point

--- config.rb
+++ config.rb
@@ -1,9 +1,11 @@
 # Require any additional compass plugins here.
+require "moz_nav/extensions/sass"
+
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "lib/moz_nav/assets/stylesheets/development"
 sass_dir = "lib/moz_nav/assets/sass"
-images_dir = "lib/moz_nav/assets/images"
+images_dir = "images/moz_nav"
 javascripts_dir = "lib/moz_nav/assets/javascripts"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
