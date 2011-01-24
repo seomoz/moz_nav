9616cb280a11688152f881a5f0192d0e2e7ab48f
Jeff Pollard <jeff.pollard@gmail.com>
2011-01-23 22:29:02 -0800

    New better config.rb file.

--- config.rb
+++ config.rb
@@ -3,8 +3,9 @@ require "moz_nav/extensions/sass"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "lib/moz_nav/assets/stylesheets/development"
-sass_dir = "lib/moz_nav/assets/sass"
+project_path = "lib/moz_nav/assets"
+css_dir = "stylesheets/development"
+sass_dir = "sass"
 images_dir = "images/moz_nav"
 javascripts_dir = "lib/moz_nav/assets/javascripts"
 # To enable relative paths to assets via compass helper functions. Uncomment:
