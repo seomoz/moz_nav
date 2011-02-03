33e2d743d2cac96bd4b0c3fcb38598a98e527f00
Jeff Pollard <jeff.pollard@gmail.com>
2011-02-02 16:20:40 -0800

    Add correct links to the user bar

--- spec/support/logged_out_user_box.rb
+++ spec/support/logged_out_user_box.rb
@@ -1,8 +1,8 @@
 shared_examples_for "logged out user box" do
   {
-    "Log in"  => "#",
+    "Log in"  => "https://#{SEOMOZ_HOST}/users/login",
     "Sign up" => "https://#{SEOMOZ_HOST}/users/register",
-    "Help"    => '#'
+    "Help"    => "http://#{SEOMOZ_HOST}/about/contact"
   }.each do |text, url|
     it "contains a '#{text}' link to #{url}" do
       header_nav_user_box.should have_link(text, url)
