988be9d7638c5da776df76ea3c4bd6052dab93ca
Myron Marston <myron.marston@gmail.com>
2011-02-15 17:09:51 -0800

    Fix user box links to properly expand the drop down.

--- spec/support/logged_in_user_box.rb
+++ spec/support/logged_in_user_box.rb
@@ -1,7 +1,7 @@
 shared_examples_for "logged in user box" do |user_id|
   {
-    "My Account" => "http://#{SEOMOZ_HOST}/users/view/#{user_id}",
-    "Help"       => "http://#{SEOMOZ_HOST}/about/contact"
+    "My Account" => "#",
+    "Help"       => "#"
   }.each do |text, url|
     it "contains a '#{text}' link to #{url}" do
       header_nav_user_box.should have_link(text, url)
