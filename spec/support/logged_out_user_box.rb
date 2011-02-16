82ad013711605e7643814c9a9efe88aa208bb635
Myron Marston <myron.marston@gmail.com>
2011-02-15 17:32:12 -0800

    Fix help link.

--- spec/support/logged_out_user_box.rb
+++ spec/support/logged_out_user_box.rb
@@ -2,7 +2,7 @@ shared_examples_for "logged out user box" do
   {
     "Log in"  => "https://#{SEOMOZ_HOST}/users/login",
     "Sign up" => "https://#{SEOMOZ_HOST}/users/register",
-    "Help"    => "http://#{SEOMOZ_HOST}/about/contact"
+    "Help"    => "#"
   }.each do |text, url|
     it "contains a '#{text}' link to #{url}" do
       header_nav_user_box.should have_link(text, url)
