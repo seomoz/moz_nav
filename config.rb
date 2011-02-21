021b2bf73030cb3dfe82633bf9d5e1faba1b2fe0
Myron Marston <myron.marston@gmail.com>
2011-02-21 12:42:13 -0800

    Ensure moz_nav is on the load path before require (so we can run rake task without bundle exec).

--- config.rb
+++ config.rb
@@ -1,4 +1,5 @@
 # Require any additional compass plugins here.
+$LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
 require "moz_nav/extensions/sass"
 
 # Set this to the root of your project when deployed:
